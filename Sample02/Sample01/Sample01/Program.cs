1db845ea719b34349e47653ed348a5cae2119ffb
zz94263 <zz94263@qq.com>
2020-07-30 17:46:53 +0800

    新增第三篇文章的示例代码

---
+++ Sample02/Sample01/Sample01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Sample01
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main方法，程序的入口方法
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)//调用下面的方法，返回一个IWebHostBuilder对象
+                .Build()//用上面返回的IWebHostBuilder对象创建一个IWebHost
+                .Run();//运行上面创建的IWebHost对象从而运行我们的Web应用程序换句话说就是启动一个一直运行监听http请求的任务
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)//使用默认的配置信息来初始化一个新的IWebHostBuilder实例
+            .ConfigureAppConfiguration((hostingContext, config) => 
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.json.(env.EnviromentName).json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Content.json", optional: false, reloadOnChange: false)//这里设置了true则有变化自动重新加载，设置false则不自动加载
+                .AddEnvironmentVariables();
+            })
+                .UseStartup<Startup>(); //为Web Host指定了Startup类
+    }
+}
